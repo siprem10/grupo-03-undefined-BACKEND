f34c14ec2a748ed7fd04a0550da8ce6471cc991e
siprem10 <siprem10@gmail.com>
2022-12-02 15:51:19 -0300

    feature(protect routes users)

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 const express = require('express');
-// const { verifyToken } = require('../middlewares/jwtValidator');
+const { verifyToken } = require('../middlewares/jwtValidator');
 const { getAll, getById, createUser, updateUser, deleteUser } = require('../controllers/users');
 
 const router = express.Router();
 
-router.get('/', /*verifyToken,*/ getAll);
-router.get('/:id', /*verifyToken,*/ getById);
+router.get('/', verifyToken, getAll);
+router.get('/:id', verifyToken, getById);
 router.post('/', createUser);
-router.put('/:id', /*verifyToken,*/ updateUser);
-router.delete('/:id', /*verifyToken,*/ deleteUser);
+router.put('/:id', verifyToken, updateUser);
+router.delete('/:id', verifyToken, deleteUser);
 
 module.exports = router;
