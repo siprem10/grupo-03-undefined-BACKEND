eeef3512290b33942a41d7f11c729511da7794b7
Ramiro Joaquin Dominguez <siprem10@gmail.com>
2022-12-02 10:16:39 -0300

    feature(add JWT middleware and util encode&decode) (#10)

---
+++ middlewares/jwtValidator.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+
+  const token = req.header("auth-token");
+
+  if(!token) {
+    return res.status(401).json({ error: "Access denied!" })
+  }
+  
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.body.user = verified;
+    next();
+
+  } catch (error) {    
+    return res.status(400).json({error: "Invalid token!"});
+  }
+}
+
+module.exports = verifyToken;
---
+++ utils/jwt.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const jwt = require("jsonwebtoken");
+
+const signToken = (any) => {
+
+  if(!any) return;
+  
+  try {
+    const signToken = jwt.sign(any, process.env.TOKEN_SECRET);
+    return signToken;
+
+  } catch (error) {    
+    throw new Error({error: "Error to sign token!"});
+  }
+}
+
+const decodeToken = (token) => {
+
+  if(!token) return;
+  
+  try {
+    const decodeToken = jwt.decode(token);
+    return decodeToken;
+
+  } catch (error) {    
+    throw new Error({error: "Error to decode token!"});
+  }
+}
