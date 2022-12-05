e7398cca215aa35be783c153081c38155bc4490e
alegb91 <93295277+alegb91@users.noreply.github.com>
2022-12-05 08:33:37 -0500

    Transactions endpoints (#17)
    * added transactions endpoints
    
    * Delete json

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const usersRouter = require('./users');
 const categoriesRouter = require("./categories");
+const transactionsRouter = require("./transactions");
 const authRouter = require('./auth');
 
 const router = express.Router();
 
 router.use("/users", usersRouter);
 router.use("/categories", categoriesRouter);
+router.use("/transactions", transactionsRouter);
 router.use("/auth", authRouter);
 
 module.exports = router;
