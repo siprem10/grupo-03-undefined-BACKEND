98bb3d6df12aef2fc86f04896ffd1fff438077ff
Luciano Piñol <97205367+Luem2@users.noreply.github.com>
2022-12-11 04:02:45 -0300

    Test/endpoints users (#26)
    * init user tests
    
    * add script test:jest
    
    * delete demotest & change file name
    
    * add config test enviroment
    
    * user testing init
    
    * finish get users & by id testing
    
    * testing post users route
    
    * reusable function validation token & refactor
    
    * init 2 describes routes
    
    * set loggin & native to false in config.js
    
    * finish user testing

--- config/config.js
+++ config/config.js
@@ -23,4 +23,15 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: 'mysql',
   },
+
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+    native: false,
+  },
 };
