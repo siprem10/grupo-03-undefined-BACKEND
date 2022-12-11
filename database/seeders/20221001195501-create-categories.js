ab950785653c587ddc780acfec6be5749b77a23f
Luciano Piñol <97205367+Luem2@users.noreply.github.com>
2022-12-11 00:25:07 -0300

    update seed & finish categories testing (#25)

--- database/seeders/20221001195501-create-categories.js
+++ database/seeders/20221001195501-create-categories.js
@@ -1,26 +1,28 @@
 'use strict';
 
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
-        const categories = [
-            {
-                name: 'Incomes',
-                description: 'Ingreso de dinero',
-                createdAt: new Date(),
-                updatedAt: new Date(),
-            },
-            {
-                name: 'Outcomes',
-                description: 'Egreso de dinero',
-                createdAt: new Date(),
-                updatedAt: new Date(),
-            },
-        ];
+  up: async (queryInterface, Sequelize) => {
+    const categories = [
+      {
+        name: 'Incomes',
+        description: 'Ingreso de dinero',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        active: true,
+      },
+      {
+        name: 'Outcomes',
+        description: 'Egreso de dinero',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        active: true,
+      },
+    ];
 
-        await queryInterface.bulkInsert('Categories', categories, {});
-    },
+    await queryInterface.bulkInsert('Categories', categories, {});
+  },
 
-    down: async (queryInterface, Sequelize) => {
-        await queryInterface.bulkDelete('Categories', null, {});
-    },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Categories', null, {});
+  },
 };
