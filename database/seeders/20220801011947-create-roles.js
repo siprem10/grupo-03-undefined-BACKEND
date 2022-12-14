6c35c5c235b434396fde89689943542f3e3b8bf5
siprem10 <siprem10@gmail.com>
2022-12-14 13:49:40 -0300

    feature(roles)

--- database/seeders/20220801011947-create-roles.js
+++ database/seeders/20220801011947-create-roles.js
@@ -9,21 +9,12 @@ module.exports = {
           name: 'Administrator',
           description:
             'Puede cambiar de roles a los usuarios y puede banearlos. Ademas de poder usar la plataforma.',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: 'Premium-User',
-          description:
-            'Puede usar la plataforma como un usuario Standard, con algunos beneficios.',
-          createdAt: new Date(),
-          updatedAt: new Date(),
+          createdAt: new Date()
         },
         {
           name: 'Standard-User',
           description: 'Puede usar la plataforma.',
-          createdAt: new Date(),
-          updatedAt: new Date(),
+          createdAt: new Date()
         },
       ],
       {}
